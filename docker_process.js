bde67ab21b05e0b655a6216a65698284c0f133c2
= <=>
2022-09-24 10:34:46 +0800

    [Feat] add child process package

--- docker_process.js
+++ docker_process.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
+const { exec } = require('child_process');
 
 async function runCommand(cmd) {
   return new Promise((resolve, reject) => {
@@ -21,11 +22,13 @@ async function compile() {
   const userCodeFile = process.env.TEST_CODE_FILE
   let compilerResult;
   try {
-    compilerResult = await runCommand(`docker run -v \$\(pwd\)/user_tmp_codes:/bunny_code/user_tmp_codes -e TOWSUMFILE=./user_tmp_codes/${userCodeFile} --rm node-tool /bunny_code/user_tmp_codes/${userCodeFile}`);
+    compilerResult = await runCommand(`docker run -v \$\(pwd\)/user_tmp_codes:/bunny_code/user_tmp_codes -e TOWSUMFILE=./user_tmp_codes/${userCodeFile} --rm sandbox /bunny_code/user_tmp_codes/twoSum.js`);
   } catch (error) {
     compilerResult = error;
   }
   return compilerResult;
 }
 
-compile();
+compile().then((result) => {
+  console.log(result);
+});
