9f055bb6a6f75ad021e832557f495dc3a0d796e4
= <=>
2022-09-24 12:51:03 +0800

    [Feat] user limited file auth

--- docker_process.js
+++ docker_process.js
@@ -21,7 +21,7 @@ async function compile() {
   const userCodeFile = process.env.TEST_CODE_FILE
   let compilerResult;
   try {
-    compilerResult = await runCommand(`docker run -v \$\(pwd\)/user_tmp_codes:/bunny_code/user_tmp_codes -e TOWSUMFILE=${userCodeFile} --rm sandbox /bunny_code/twoSum.js`);
+    compilerResult = await runCommand(`docker run -v \$\(pwd\)/${userCodeFile}:/bunny_code/user_tmp_codes -e TOWSUMFILE=${userCodeFile} --rm sandbox /bunny_code/twoSum.js`);
   } catch (error) {
     compilerResult = error;
   }
--- twoSum.js
+++ twoSum.js
@@ -1,7 +1,16 @@
-const { twoSum }  = require(process.env.TOWSUMFILE);
+require('dotenv').config()
+const { twoSum }  = require(`${process.env.TWOSUMFILE}`);
+
+const compilerResults = []
+compilerResults.push(twoSum([2, 7, 11, 15], 9))
+compilerResults.push(twoSum([2, 7, 11, 15], 26))
+compilerResults.push(twoSum([1, 1, 1, 1], 2))
+compilerResults.push(twoSum([-1], 10));
+
+console.log(compilerResults);
 
 //Test two sum
-console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
-console.log(twoSum([2, 7, 11, 15], 26)); // [2,3]
-console.log(twoSum([1, 1, 1, 1], 2)); //[0, 1]
-console.log(twoSum([-1], 10)); // -1
+// console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
+// console.log(twoSum([2, 7, 11, 15], 26)); // [2,3]
+// console.log(twoSum([1, 1, 1, 1], 2)); //[0, 1]
+// console.log(twoSum([-1], 10)); // -1
